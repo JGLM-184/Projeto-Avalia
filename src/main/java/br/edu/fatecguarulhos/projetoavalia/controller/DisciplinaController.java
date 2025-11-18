97165217d7484f7b3432aaef7b0292e516b2bea1
unknown <viniciushgevil@gmail.com>
2025-11-18 13:24:33 -0300

    corrigida api para disciplinas no cadastro de usuario

--- src/main/java/br/edu/fatecguarulhos/projetoavalia/controller/DisciplinaController.java
+++ src/main/java/br/edu/fatecguarulhos/projetoavalia/controller/DisciplinaController.java
@@ -1,5 +1,7 @@
 package br.edu.fatecguarulhos.projetoavalia.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.fatecguarulhos.projetoavalia.dto.DisciplinaDTO;
+import br.edu.fatecguarulhos.projetoavalia.model.entity.Disciplina;
 import br.edu.fatecguarulhos.projetoavalia.service.CursoService;
 import br.edu.fatecguarulhos.projetoavalia.service.DisciplinaService;
 
@@ -57,5 +62,11 @@ public class DisciplinaController {
             redirectAttributes.addFlashAttribute("errorMessage", "Erro ao excluir a disciplina.");
         }
         return "redirect:/disciplinas/painel";
+    }
+    
+    @ResponseBody
+    @GetMapping("/por-cursos")
+    public List<Disciplina> listarDisciplinasPorCursos(@RequestParam List<Integer> cursosIds) {
+        return disciplinaService.buscarPorCursosIds(cursosIds);
     }
 }
